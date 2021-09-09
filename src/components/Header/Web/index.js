32e5477cc2aa37c58dbab543eea3a842bba85ebb
Kevin Johnson <kevin.michael805@gmail.com>
2021-09-09 15:03:23 -0600

    web header links complete

--- src/components/Header/Web/index.js
+++ src/components/Header/Web/index.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import './web.css'
 
 function Web() {
     return (
-        <div>
-            This is Web
+        <div className='web'>
+            <div className='web-option'>
+                <a href='#project'>
+                    <i class="fi-rr-microphone option-icon"></i> Projects
+                </a>
+            </div>
+            <div className='web-option'>
+                <a href='#skills'> 
+                    <i class="fi-rr-inbox option-icon"></i> Skills
+                </a>
+            </div>
+            <div className='web-option'>
+                <a href='#education'> 
+                    <i class="fi-rr-forward option-icon"></i> Education
+                </a>
+            </div>
+            <div className='web-option'>
+                <a href='#contact'> 
+                    <i class="fi-rr-duplicate option-icon"></i> Contact
+                </a>
+            </div>
         </div>
     )
 }
