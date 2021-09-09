156bcf43cc15237d4e253641deec5ad646caca3c
Kevin Johnson <kevin.michael805@gmail.com>
2021-09-09 12:25:23 -0600

    Header Body Footer set up.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import Home from './components/Home'
+import Home from './components/Home/index'
+
 
 function App() {
   return ( 
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Home() {
+function Footer() {
     return (
         <div>
-            Home page
+            This is my Footer
         </div>
     )
 }
 
-export default Home
+export default Footer
