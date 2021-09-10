4983b177b4f43a4d125bd0e2e4cbcbf7151cd6d9
Kevin Johnson <kevin.michael805@gmail.com>
2021-09-10 10:06:56 -0600

    navbar completed web and mobile

--- src/components/Header/Mobile/index.js
+++ src/components/Header/Mobile/index.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import './mobile.css'
 
-function Mobile() {
+function Mobile({isOpen, setIsOpen}) {
     return (
-        <div>
-            This is Mobile
+        <div className="mobile">
+            <div className="close-icon" onClick={() => setIsOpen(!isOpen)}> 
+            <i class="fi-rr-copy"></i>
+            </div>
+            <div className="mobile-options">
+            <div className='mobile-option'>
+                <a href='#project'>
+                    <i class="fi-rr-microphone option-icon"></i> Projects
+                </a>
+            </div>
+            <div className='mobile-option'>
+                <a href='#skills'> 
+                    <i class="fi-rr-inbox option-icon"></i> Skills
+                </a>
+            </div>
+            <div className='mobile-option'>
+                <a href='#education'> 
+                    <i class="fi-rr-forward option-icon"></i> Education
+                </a>
+            </div>
+            <div className='mobile-option'>
+                <a href='#contact'> 
+                    <i class="fi-rr-duplicate option-icon"></i> Contact
+                </a>
+            </div>
+            </div>
         </div>
     )
 }
