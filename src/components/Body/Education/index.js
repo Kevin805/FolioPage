bed36916b49c304781e196cc9a13508c05fb2448
Kevin Johnson <kevin.michael805@gmail.com>
2021-09-10 10:18:43 -0600

    Body components created.

---
+++ src/components/Body/Education/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Education() {
+    return (
+        <div>
+           This is Education 
+        </div>
+    )
+}
+
+export default Education
--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import './body.css'
+import About from './About/index'
+import Projects from './Projects/index'
+import Skills from './Skills/index'
+import Education from './Education/index'
+import Contact from './Contact/index'
 
 function Body() {
     return (
-        <div>
-            This is my Body
+        <div className="body">
+            <section id="about">
+                <About />
+            </section>
+            <section id="projects">
+                <Projects />
+            </section>
+            <section id="skills">
+                <Skills />
+            </section>
+            <section id="education">
+                <Education />
+            </section>
+            <section id="contact">
+                <Contact />
+            </section>
         </div>
     )
-}
+};
 
 export default Body
