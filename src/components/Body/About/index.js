7279c7281e1a6a412d804e59c04ff86c43329706
Kevin Johnson <kevin.michael805@gmail.com>
2021-09-13 09:45:22 -0600

    projects started.

--- src/components/Body/About/index.js
+++ src/components/Body/About/index.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import SocialContact from '../../common/social-contact'
+import './about.css'
 
 function About() {
     return (
-        <div>
-            This is About
+        <div className="about">
+            <div className="about-top">
+                <div className="about-info">
+                Hello 👋🏼, my name is <span className="info-name">Kevin!</span>
+                <br /> I'm a passionate problem-solver, 
+                <br /> creator of colorful content, 
+                <br /> and a life long learner.  
+                </div>
+                <div className="about-photo">
+                    <img src={require('../../../assets/coding.png').default} className='picture'/>
+                </div>
+            </div>
+            <SocialContact />
         </div>
     )
 }
---
+++ src/components/data/social.js
@@ -0,0 +1,22 @@
+export const SocialData=[
+    {
+        platform:"GitHub",
+        link:"www.github.com/Kevin805",
+        icon: require('../../assets/icons/github.png').default,
+    },
+    {
+        platform:"Linkedin",
+        link:"www.linkedin.com/in/kevin-johnson805",
+        icon: require('../../assets/icons/linkedin.png').default,
+    },
+    {
+        platform:"Twitter",
+        link:"www.twitter.com/KevinJayGene",
+        icon: require('../../assets/icons/twitter.png').default,
+    },
+    {
+        platform:"Instagram",
+        link:"www.instagram.com/kevinjaygene",
+        icon: require('../../assets/icons/instagram.png').default,
+    },
+];
